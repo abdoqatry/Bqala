fe49ec9be34105790e250edf7942f888ce114cf0
abdelhamed <abdoqatry2@gmail.com>
2019-05-24 13:22:36 +0200

    Initial Commit

---
+++ Bqala/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bqala
+//
+//  Created by Moe on 5/24/19.
+//  Copyright © 2019 Moe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
